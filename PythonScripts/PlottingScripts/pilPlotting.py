29358e05d560a4910e5790a610021419d812e6f7
Tasman Grinnell <tgrin@iastate.edu>
2024-08-21 00:48:06 -0500

    Started trying to work on PIL Image creation (attempting to creat an image of x by x pixels instead of graphing on a graph using seaborn)

--- PythonScripts/PlottingScripts/pilPlotting.py
+++ PythonScripts/PlottingScripts/pilPlotting.py
@@ -6,13 +6,8 @@
 #
 
 import pandas
+import scipy.misc
 from PIL import Image
-
-# Colors:
-# Black         - rgb(0, 0, 0) ~~ it. 1000
-# Light brown   - rgb(255, 204, 153) ~~ 500
-# Fuschia       - rgb(255, 0, 255) ~~ 1
-
 
 def main() :
     # Process input
@@ -25,9 +20,28 @@ def main() :
     # PIL.Image.new(mode: str, size: tuple[int, int], color: float | tuple[float, ...] | str | None = 0) → Image[source]
     # use mode = RGB
 
+    #list_of_pixels = list(im.getdata())
+    # Do something to the pixels...
+    # im2 = Image.new(im.mode, im.size)
+    # im2.putdata(list_of_pixels)
+
     imageSize = [csv.sizeX(0), csv.sizeY(0)]
 
     newIm = Image.new("RGB", )
+
+    return 0
+
+# Colors:
+# Black         - rgb(0, 0, 0) ~~ it. 1000
+# Light brown   - rgb(255, 204, 153) ~~ 500
+# Fuschia       - rgb(255, 0, 255) ~~ 1
+
+def processIteration() :
+
+    # Scale evenly from light brown to black from 500-1000
+
+    # Scale evenly from Fuschia to light brown  
+
 
     return 0
 
